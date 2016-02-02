766f138690df37cc7d421b887954691e7ce3cb04
Mohamed <moh241eng@gmail.com>
2016-02-02 16:09:24 +0200

    update

--- app/src/main/java/firstapp/its/com/myfirstapp/MainActivity.java
+++ app/src/main/java/firstapp/its/com/myfirstapp/MainActivity.java
@@ -4,14 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
-
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.btn);
+
     }
 
 
